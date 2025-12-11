dec807a0e254d167069e1632fafcf02734e7d1e3
Sebastian <seba.luceroguzman@gmail.com>
2025-12-11 14:50:39 -0300

    First commit

---
+++ src/main/java/com/colegio/aplicacionapi/AplicacionapiApplication.java
@@ -0,0 +1,13 @@
+package com.colegio.aplicacionapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AplicacionapiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AplicacionapiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/colegio/aplicacionapi/AplicacionapiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.colegio.aplicacionapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AplicacionapiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
