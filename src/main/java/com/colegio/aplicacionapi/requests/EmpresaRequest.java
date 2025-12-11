5bd49b16bbef7afd132f6f2169434ce6d9ee9864
Sebastian <seba.luceroguzman@gmail.com>
2025-12-11 20:10:13 -0300

    avance controller

---
+++ src/main/java/com/colegio/aplicacionapi/requests/EmpresaRequest.java
@@ -0,0 +1,11 @@
+package com.colegio.aplicacionapi.requests;
+
+import lombok.Data;
+
+@Data
+public class EmpresaRequest {
+    private String nombre;
+    private String direccion;
+    private String telefono;
+}
+
--- src/main/java/com/colegio/aplicacionapi/responses/EmpresaResponse.java
+++ src/main/java/com/colegio/aplicacionapi/responses/EmpresaResponse.java
@@ -4,6 +4,7 @@ import lombok.Data;
 
 @Data
 public class EmpresaResponse {
+
     private String id;
     private String nombre;
     private String direccion;   
